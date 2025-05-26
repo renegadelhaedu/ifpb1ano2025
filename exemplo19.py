2c805894da5cb452a65905a35ecb9f7ea89f2e8a
renegadelhaedu <renegadelha.edu@gmail.com>
2025-05-26 13:43:31 -0300

    aula 26/05

---
+++ exemplo19.py
@@ -0,0 +1,21 @@
+#estrutura de decisão simples: if else
+#if -> se
+#else -> senao
+
+#operadores relacionais
+nomeEscolhido = input('digite seu nome')
+
+if nomeEscolhido == 'maria eduarda':
+    print('voce é uma aluna do 1 ano')
+else:
+    print('voce nao é nossa amiga maria eduarda e eu nao '
+          'faço ideia de qual é sua turma')
+
+x = 2 + 5
+
+
+
+
+
+
+
