cf5beb8882f56acf62f9a3f6b08275e8dbbd03ef
renegadelhaedu <renegadelha.edu@gmail.com>
2025-12-01 15:08:21 -0300

    aula 0112

---
+++ exemplo98.py
@@ -0,0 +1,12 @@
+'''
+1-lista  (estrutura de dados)
+2-funções (organizar blocos de código)
+    - sem parâmetros sem retorno
+    - com parâmetros sem retorno
+    - sem parâmetros com retorno
+    - com parâmetros com retorno
+
+3- modularizar seu código (pacotes de funções /importar)
+-> projeto (funcionalidades)
+
+'''
